ea34a0f00597fefb3d7b453458d9dcae6fe35381
dorlegbs <gabidorle@gmail.com>
2026-04-28 15:32:13 -0300

    Update gabi.py

--- gabi.py
+++ gabi.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title("Tetste ECMI 2")
-st.write("Esse é o meu texto")
+st.title("PIRULITOS")
+st.write("SÃO DOCINHOS")
 
 st.image("https://github.com/dorlegbs/Project_1/blob/main/pirulitos.webp?raw=true")
          
+st.inputbox
