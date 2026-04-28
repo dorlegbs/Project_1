6787a5bb1ca5d31e88f6d4d9e488614338bd55b6
dorlegbs <gabidorle@gmail.com>
2026-04-28 15:38:59 -0300

    Update gabi.py

--- gabi.py
+++ gabi.py
@@ -5,4 +5,6 @@ st.write("SÃO DOCINHOS")
 
 st.image("https://github.com/dorlegbs/Project_1/blob/main/pirulitos.webp?raw=true")
          
-st.inputbox
+sabor = st.text_input('Digite o seu sabor preferido')
+if nome:
+         st.write(sabor, "é um ótimo sabor de pirulito!")
