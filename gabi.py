2d85c0373287c231117fe94df9f24e61e04464ad
dorlegbs <gabidorle@gmail.com>
2026-04-28 15:28:22 -0300

    Update gabi.py

--- gabi.py
+++ gabi.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Tetste ECMI 2")
 st.write("Esse é o meu texto")
 
-st.image("https://www.mercadoce.com/pirulito-psicodelico-colorido-600g-com-20-un-arco-iris?srsltid=AfmBOorhdxLuy2a3sf7YU6CuYurbj5FE2IQSx4KAd9mHvieIfrljb9vO")
+st.image("https://github.com/dorlegbs/Project_1/blob/main/pirulitos.webp?raw=true")
          
